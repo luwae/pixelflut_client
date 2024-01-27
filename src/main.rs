239a966ded2db66cd6690ca451d23f56c1d00244
luwae <l.waeldele@t-online.de>
2024-01-27 22:52:52 +0100

    floyd-steinberg 0 or 255 in every color channel

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
-use fastrand;
 
 mod primitive;
 use primitive::{Pixel, Rect};
-
-mod mandel;
-
-mod barnsley;
-
-mod firework;
 
 #[derive(Debug)]
 struct ServerInfo {
@@ -19,37 +12,28 @@ struct ServerInfo {
     send_buffer_size: u32,
 }
 
-fn server_info(stream: &mut TcpStream) -> std::io::Result<ServerInfo> {
+fn decode_u32(data: &[u8]) -> u32 {
+    (data[0] as u32)
+        | ((data[1] as u32) << 8)
+        | ((data[2] as u32) << 16)
+        | ((data[3] as u32) << 24)
+}
+
+fn command_info(stream: &mut TcpStream) -> std::io::Result<ServerInfo> {
     let mut data = [0u8; 8];
     data[0] = b'I';
     stream.write_all(&data[..])?;
     let mut response = [0u8; 16];
     stream.read_exact(&mut response[..])?;
-    let width: u32 = (response[0] as u32)
-        | ((response[1] as u32) << 8)
-        | ((response[2] as u32) << 16)
-        | ((response[3] as u32) << 24);
-    let height: u32 = (response[4] as u32)
-        | ((response[5] as u32) << 8)
-        | ((response[6] as u32) << 16)
-        | ((response[7] as u32) << 24);
-    let recv_buffer_size: u32 = (response[8] as u32)
-        | ((response[9] as u32) << 8)
-        | ((response[10] as u32) << 16)
-        | ((response[11] as u32) << 24);
-    let send_buffer_size: u32 = (response[12] as u32)
-        | ((response[13] as u32) << 8)
-        | ((response[14] as u32) << 16)
-        | ((response[15] as u32) << 24);
+    let width = decode_u32(&response[0..4]);
+    let height = decode_u32(&response[4..8]);
+    let recv_buffer_size = decode_u32(&response[8..12]);
+    let send_buffer_size = decode_u32(&response[12..16]);
     Ok(ServerInfo { width, height, recv_buffer_size, send_buffer_size })
 }
 
-fn pixel_write_multi(colors: &[(u8, u8, u8)], rect: Rect, stream: &mut TcpStream) -> std::io::Result<()> {
-    assert!(rect.w > 0 && rect.h > 0);
-    assert!(colors.len() == rect.w * rect.h);
-    let mut data: Box<[u8; 1024]> = Box::new([0; 1024]);
-    // first round: write actual command
-    data[0] = b'p';
+fn encode_rect(rect: Rect, data: &mut [u8]) {
+    // skip first byte
     data[1] = rect.x as u8;
     data[2] = (rect.x >> 8) as u8;
     data[3] = rect.y as u8;
@@ -57,11 +41,44 @@ fn pixel_write_multi(colors: &[(u8, u8, u8)], rect: Rect, stream: &mut TcpStream
     data[5] = rect.w as u8;
     data[6] = rect.h as u8;
     data[7] = ((rect.w >> 8) & 0x0f) as u8 | ((rect.h >> 4) & 0xf0) as u8;
+}
+
+fn command_rectangle_get(colors: &mut [(u8, u8, u8)], rect: Rect, stream: &mut TcpStream) -> std::io::Result<()> {
+    assert!(colors.len() == rect.w * rect.h);
+    let mut command: [u8; 8] = [0; 8];
+    command[0] = b'g';
+    encode_rect(rect, &mut command[..]);
+    stream.write_all(&command[..])?;
+    // receive pixels
+    let mut data: Box<[u8; 1024]> = Box::new([0; 1024]);
+    let mut num_bytes_to_read: usize = rect.w * rect.h * 4;
+    let mut pixel_idx = 0;
+    while num_bytes_to_read > 0 {
+        let mut read_size = num_bytes_to_read;
+        if read_size > 1024 {
+            read_size = 1024;
+        }
+        stream.read_exact(&mut data[0..read_size])?;
+        num_bytes_to_read -= read_size;
+        for i in (0..read_size).step_by(4) {
+            colors[pixel_idx] = (data[i + 0], data[i + 1], data[i + 2]);
+            pixel_idx += 1;
+        }
+    }
+    Ok(())
+}
+
+fn command_rectangle_print(colors: &[(u8, u8, u8)], rect: Rect, stream: &mut TcpStream) -> std::io::Result<()> {
+    assert!(colors.len() == rect.w * rect.h);
+    let mut data: Box<[u8; 1024]> = Box::new([0; 1024]);
+    // first round: write actual command
+    data[0] = b'p';
+    encode_rect(rect, &mut data[0..8]);
     let mut data_fill_start: usize = 8;
     let mut pixel_idx = 0;
     while pixel_idx < colors.len() {
         // fill buffer
-        while data_fill_start < 1024 && pixel_idx < colors.len() {
+        while data_fill_start <= 1024 - 4 && pixel_idx < colors.len() {
             let col = colors[pixel_idx];
             data[data_fill_start] = col.0;
             data[data_fill_start + 1] = col.1;
@@ -76,190 +93,73 @@ fn pixel_write_multi(colors: &[(u8, u8, u8)], rect: Rect, stream: &mut TcpStream
     Ok(())
 }
 
-fn pixel_write_multi_onecolor(color: (u8, u8, u8), rect: Rect, stream: &mut TcpStream) -> std::io::Result<()> {
-    assert!(rect.w > 0 && rect.h > 0);
-    let mut data: Box<[u8; 1024]> = Box::new([0; 1024]);
-    // first round: write actual command
-    data[0] = b'p';
-    data[1] = rect.x as u8;
-    data[2] = (rect.x >> 8) as u8;
-    data[3] = rect.y as u8;
-    data[4] = (rect.y >> 8) as u8;
-    data[5] = rect.w as u8;
-    data[6] = rect.h as u8;
-    data[7] = ((rect.w >> 8) & 0x0f) as u8 | ((rect.h >> 4) & 0xf0) as u8;
-    let mut data_fill_start: usize = 8;
-    let mut pixel_idx = 0;
-    while pixel_idx < rect.w * rect.h {
-        // fill buffer
-        while data_fill_start < 1024 && pixel_idx < rect.w * rect.h {
-            data[data_fill_start] = color.0;
-            data[data_fill_start + 1] = color.1;
-            data[data_fill_start + 2] = color.2;
-            data[data_fill_start + 3] = 0;
-            pixel_idx += 1;
-            data_fill_start += 4;
-        }
-        stream.write_all(&data[0..data_fill_start])?; // buffer may not be full in last round
-        data_fill_start = 0; // reset buffer
+fn add_delta_single(delta: i32, top: i32, bot: i32, base: u8) -> u8 {
+    let m: i32 = delta * top / bot + (base as i32);
+    if (m < 0) {
+        0
+    } else if (m > 255) {
+        255
+    } else {
+        m as u8
     }
-    Ok(())
 }
 
-fn pixel_write(px: &Pixel, stream: &mut TcpStream) -> std::io::Result<()> {
-    // let data = format!("PX {} {} {:02x}{:02x}{:02x}\n", px.x, px.y,
-        // px.color.0, px.color.1, px.color.2);
-    let mut data = [0u8; 8];
-    data[0] = b'P';
-    data[1] = px.x as u8;
-    data[2] = (px.x >> 8) as u8;
-    data[3] = px.y as u8;
-    data[4] = (px.y >> 8) as u8;
-    data[5] = px.color.0;
-    data[6] = px.color.1;
-    data[7] = px.color.2;
-    if stream.write(&data[..])? != 8 {
-        panic!("write != 8");
-    }
-    Ok(())
+fn add_delta(delta: (i32, i32, i32), top: i32, bot: i32, base: &mut (u8, u8, u8)) {
+    base.0 = add_delta_single(delta.0, top, bot, base.0);
+    base.1 = add_delta_single(delta.1, top, bot, base.1);
+    base.2 = add_delta_single(delta.2, top, bot, base.2);
 }
 
-fn pixel_read(px: &mut Pixel, stream: &mut TcpStream) -> std::io::Result<()> {
-    let mut data = [0u8; 8];
-    data[0] = b'G';
-    data[1] = px.x as u8;
-    data[2] = (px.x >> 8) as u8;
-    data[3] = px.y as u8;
-    data[4] = (px.y >> 8) as u8;
-    if stream.write(&data[..])? != 8 {
-        panic!("write != 8");
+fn approx_single(col: u8) -> (u8, i32) {
+    if (col < 128) {
+        (0, col as i32)
+    } else {
+        (255, col as i32 - 255)
     }
-    if stream.read(&mut data[..])? != 4 {
-        panic!("read != 4");
-    }
-    px.color.0 = data[0];
-    px.color.1 = data[1];
-    px.color.2 = data[2];
-    Ok(())
 }
 
-fn pixel_read_multi(colors: &mut Vec<(u8, u8, u8)>, rect: Rect, stream: &mut TcpStream) -> std::io::Result<()> {
-    let mut command: [u8; 8] = [0; 8];
-    command[0] = b'g';
-    command[1] = rect.x as u8;
-    command[2] = (rect.x >> 8) as u8;
-    command[3] = rect.y as u8;
-    command[4] = (rect.y >> 8) as u8;
-    command[5] = rect.w as u8;
-    command[6] = rect.h as u8;
-    command[7] = ((rect.w >> 8) & 0x0f) as u8 | ((rect.h >> 4) & 0xf0) as u8;
-    stream.write_all(&command[..])?;
-    // receive pixels
-    let mut data: Box<[u8; 1024]> = Box::new([0; 1024]);
-    let mut num_bytes_to_read: usize = rect.w * rect.h * 4;
-    while num_bytes_to_read > 0 {
-        let mut read_size = num_bytes_to_read;
-        if read_size > 1024 {
-            read_size = 1024;
-        }
-        stream.read_exact(&mut data[0..read_size])?;
-        num_bytes_to_read -= read_size;
-        for i in (0..read_size).step_by(4) {
-            colors.push((data[i + 0], data[i + 1], data[i + 2]));
-        }
-    }
-    Ok(())
+fn approx(col: (u8, u8, u8)) -> ((u8, u8, u8), (i32, i32, i32)) {
+    let (ar, dr) = approx_single(col.0);
+    let (ag, dg) = approx_single(col.1);
+    let (ab, db) = approx_single(col.2);
+    ((ar, ag, ab), (dr, dg, db))
 }
 
-/*
-fn rec(stream: &mut TcpStream, sx: usize, sy: usize, idepth: usize) {
-    if idepth == 10 {
-        return;
-    }
-    let size = 1 << (10 - idepth);
-    let col = ((255 * idepth) / 10) as u8;
-    let mut p = Pixel { x: 0, y: 0, color: (col, 0, col) };
-    for x in sx..(sx+size) {
-        for y in sy..(sy+size) {
-            p.x = x;
-            p.y = y;
-            pixel_write(&p, stream).unwrap();
-        }
-    }
-    rec(stream, sx + (size/2), sy, idepth + 1);
-    rec(stream, sx, sy + (size/2), idepth + 1);
-}
-*/
-
-fn random_walk(p: &mut Pixel) {
-    let dir = fastrand::usize(0..4);
-    match dir {
-        0 => if p.y == 0 { p.y = 1023; } else { p.y -= 1; },
-        1 => if p.x == 1023 { p.x = 0; } else { p.x += 1; },
-        2 => if p.y == 1023 { p.y = 0; } else { p.y += 1; },
-        3 => if p.x == 0 { p.x = 1023; } else { p.x -= 1; },
-        _ => panic!("dir"),
-    }
-}
+/*        *  7/16
+ * 3/16 5/16 1/16
+ */
 
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:1337")?;
-    // let pxs = mandel::draw(-1.5, 1.5, -1.5, 1.5, 1024, 1024);
-    /*
-    loop {
-        let pxs = barnsley::barnsley_vec(1000);
-        for px in &pxs {
-            pixel_write(&px, &mut stream)?;
-        }
-    }
-    */
-    /*
-    let mut px = Pixel { x: 0, y: 0, color: (0, 0, 0) };
-    loop {
-        for x in 0..256 {
-            for y in 0..256 {
-                px.x = x;
-                px.y = y;
-                pixel_read(&mut px, &mut stream)?;
-                px.color.0 = 255u8 - px.color.0;
-                px.color.1 = 255u8 - px.color.1;
-                px.color.2 = 255u8 - px.color.2;
-                pixel_write(&px, &mut stream)?;
+    let info = command_info(&mut stream)?;
+    let mut colors = vec![(0u8, 0u8, 0u8); (info.width as usize) * (info.height as usize)];
+    command_rectangle_get(&mut colors[..], Rect { x: 0, y: 0, w: info.width as usize, h: info.height as usize }, &mut stream)?;
+    
+    let w = info.width as usize;
+    let h = info.height as usize;
+    
+    for y in 0usize..h {
+        for x in 0usize..w {
+            let (new_col, delta) = approx(colors[y * w + x]);
+            colors[y * w + x] = new_col;
+            // distribute delta
+            if x + 1 < w {
+                add_delta(delta, 7, 16, &mut colors[y * w + (x + 1)]);
+            }
+            if x != 0 && y + 1 < h {
+                add_delta(delta, 3, 16, &mut colors[(y + 1) * w + (x - 1)]);
+            }
+            if y + 1 < h {
+                add_delta(delta, 5, 16, &mut colors[(y + 1) * w + x]);
+            }
+            if x + 1 < w && y + 1 < h {
+                add_delta(delta, 1, 16, &mut colors[(y + 1) * w + (x + 1)]);
             }
         }
     }
-    */
-    /*
-    let rect = Rect { x: 0, y: 0, w: 512, h: 512 };
-    pixel_write_multi_onecolor((255, 0, 0), rect, &mut stream)?;
-    */
-    /*
-    let mut px = Pixel { x: 0, y: 0, color: (0, 0, 255) };
-    for y in 0..512 {
-        for x in 0..512 {
-            px.x = x;
-            px.y = y;
-            pixel_write(&px, &mut stream)?;
-        }
-    }
-    */
-    let rect = Rect { x: 200, y: 100, w: 10, h: 10 };
-    let mut colors: Vec<(u8, u8, u8)> = Vec::new();
-    for i in 0u8..100u8 {
-        colors.push((i, i, i));
-    }
-    pixel_write_multi(&colors[..], rect, &mut stream)?;
-
-    colors = Vec::new();
-    let rect = Rect { x: 0, y: 0, w: 512, h: 512 };
-    pixel_read_multi(&mut colors, rect, &mut stream)?;
-    for color in &mut colors[..] {
-        color.0 = 255u8 - color.0;
-        color.1 = 255u8 - color.1;
-        color.2 = 255u8 - color.2;
-    }
-    pixel_write_multi(&colors[..], rect, &mut stream)?;
-    println!("{:?}", server_info(&mut stream)?);
-
+    
+    command_rectangle_print(&colors[..], Rect { x: 0, y: 0, w: info.width as usize, h: info.height as usize }, &mut stream)?;
+    
+    println!("{:?}", info);
     Ok(())
 }
